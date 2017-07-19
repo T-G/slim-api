6a5ca0bea7a11c736e24ae49a39ba2b3fb7427bd
T-G <pronoygomes@yahoo.com>
2017-07-19 13:55:48 +0100

    add mysql db connection and CRUD functionality

--- app/api/books.php
+++ app/api/books.php
@@ -1,4 +1,79 @@
 <?php
-$app->get('/api/books', function(){
-    echo("Welcome to books page");
+
+// List all books
+$app->get('/api/books', function () {
+
+    require_once("dbconnect.php");
+
+    $query = "select * from books";
+
+    $result = $mysqli->query($query);
+
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    if (isset($data)) {
+        header('Content-Type: x-www-form-uelencoded');
+        echo json_encode($data);
+    }
+});
+
+// DISPLAY a single book by id
+$app->get('/api/books/{id}', function($request, $response, $args) {
+    require_once("dbconnect.php");
+    $id = $args['id'];
+    $query = "select * from books where id=$id";
+    $result = $mysqli->query($query);
+
+    if($result){
+        $data[] = $result->fetch_assoc();
+        header('Content-Type: x-www-form-uelencoded');
+        echo json_encode($data, JSON_PRETTY_PRINT);
+    }
+});
+
+// POST data and create a new record
+$app->post('/api/books', function($request, $response, $args) {
+    require_once("dbconnect.php");
+    $query = "INSERT INTO `books` (`book_title`, `author`, `amazon_url`) VALUES (?,?,?)";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("sss", $book_title, $author, $amazon_url);
+
+    $book_title = $request->getParsedBody()['book_title'];
+    $author = $request->getParsedBody()['author'];
+    $amazon_url = $request->getParsedBody()['amazon_url'];
+
+    $stmt->execute();
+    echo("done");
+    
+});
+
+
+
+// POST data and create a new record
+$app->put('/api/books/{id}', function($request, $response, $args) {
+    require_once("dbconnect.php");
+    $id = $args['id'];
+    $query = "UPDATE `books` SET `book_title` = ?, `author` = ?, `amazon_url` = ? WHERE `books`.`id` = $id";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("sss", $book_title, $author, $amazon_url);
+
+    $book_title = $request->getParsedBody()['book_title'];
+    $author = $request->getParsedBody()['author'];
+    $amazon_url = $request->getParsedBody()['amazon_url'];
+
+    $stmt->execute();
+    echo("done");
+    
+});
+
+// POST data and create a new record
+$app->delete('/api/books/{id}', function($request, $response, $args) {
+    require_once("dbconnect.php");
+    $id = $args['id'];
+    $query = "delete from `books` WHERE `books`.`id` = $id";
+    $mysqli->query($query);
+    
+    echo("done");
+    
 });
---
+++ app/api/dbconnect.php
@@ -0,0 +1,9 @@
+<?php 
+$cs = [
+    "host" =>  "localhost",
+    "username" => "root",
+    "password" => "",
+    "db" => "myslimsite"
+];
+
+$mysqli = new mysqli($cs["host"], $cs["username"], $cs["password"], $cs["db"]);
